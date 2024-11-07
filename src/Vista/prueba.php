07a19fd843464a20ea823e81d04568cc6dec9f6f
DiegoAvila1 <diegoavilaaa0@gmail.com>
2024-11-07 19:22:49 -0300

    Co-authored-by: Juan Cruz Geslowski <juancruzgw@users.noreply.github.com>

---
+++ src/Vista/prueba.php
@@ -0,0 +1,12 @@
+<?php
+
+require_once "../../../vendor/autoload.php";
+use Whoops\Run;
+use Whoops\Handler\PrettyPageHandler;
+$whoops = new Run();
+$whoops->pushHandler(new PrettyPageHandler());
+$whoops->register();
+
+echo $hola;
+
+?>
