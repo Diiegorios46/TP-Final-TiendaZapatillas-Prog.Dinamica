d12ce201a284bf569ddc6809b72230a45744b72d
Balenbv <valentinbv29@gmail.com>
2024-11-15 17:19:46 -0300

    Reorganizar la inicialización de sesión en el archivo de configuración

--- config.php
+++ config.php
@@ -15,15 +15,16 @@ $PROYECTO ='TpFinal-TiendaZapatillas';
 $ROOT = $_SERVER['DOCUMENT_ROOT']."/$PROYECTO/";
 
 include_once($ROOT.'src/Utils/utils.php');
-$session = new Session();
 include_once($ROOT.'vendor/autoload.php');
+include_once($ROOT.'src/Control/Session.php');
 
 // Variable que define la pagina de autenticacion del proyecto
 $INICIO = "Location:http://".$_SERVER['HTTP_HOST']."/$PROYECTO/vista/login/login.php";
 
 // variable que define la pagina principal del proyecto (menu principal)
 $PRINCIPAL = "Location:http://".$_SERVER['HTTP_HOST']."/$PROYECTO/principal.php";
-$session->setearUnDato(['nombreDato'=>'ROOT','dato'=>$ROOT]);
+$session = new Session();
+$session->setearUnDato( ['nombreDato'=>'ROOT','dato'=>$ROOT] );
 
 
 ?>
