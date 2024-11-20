1505da30613e518e5fd06c35a88138cbea61ac20
Balenbv <valentinbv29@gmail.com>
2024-11-20 15:38:13 -0300

    Agregar método obtenerDatos en abmCompraEstadoTipo y actualizar actionHistorial para incluir detalles de compra

--- src/Vista/History/actionHistorial.php
+++ src/Vista/History/actionHistorial.php
@@ -8,20 +8,27 @@ $usuario = $session->getUsuario();
 $abmProducto = new abmProducto();
 $abmCompra = new abmCompra();
 $abmCompraEstado = new abmCompraEstado();
+$abmCompraEstadoTipo = new abmCompraEstadoTipo();
 $abmCompraItem = new abmCompraItem();
 
 $historial = [];
 
 foreach($abmCompra->obtenerDatos(['idusuario' => $usuario['idusuario']]) as $compra){
-    $compraEstado = $abmCompraEstado->obtenerDatos(['idcompra' => $compra['idcompra']]);
-    if($compraEstado[0] != null && $compraEstado['idcompraestadotipo'] == 3){
+    $compraEstado = $abmCompraEstado->obtenerDatos(['idcompra' => $compra['idcompra']])[0];
+    if($compraEstado != null){
+        $compraDatos['estadotipo'] = $abmCompraEstadoTipo->obtenerDatos(['idcompraestadotipo' => $compraEstado['idcompraestadotipo']])[0]['cetdescripcion'];
         $compraDatos['cefechaini'] = $compraEstado['cefechaini'];
         $compraDatos['cefechafin'] = $compraEstado['cefechafin'];
+        $compraDatos['idcompra'] = $compra['idcompra'];
+        $compraItems = $abmCompraItem->obtenerDatos(['idcompra' => $compra['idcompra']])[0];
+        $compraDatos['cicantidad'] = $compraItems['cicantidad'];
+        $producto = $abmProducto->obtenerDatos(['idproducto' => $compraItems['idproducto']])[0];
+        $compraDatos['prodetalle'] = $producto['prodetalle'];
+        $compraDatos['proprecio'] = $producto['proprecio'];
+        $compraDatos['pronombre'] = $producto['pronombre'];
+        $historial[] = $compraDatos;
     }
-
-
 }
 
 
-verEstructura($compraDatos);
-// pronombre, cefechaini, cefechafin, prodetalle, proprecio, cicantidad, idcompra   
+verEstructura($historial);
