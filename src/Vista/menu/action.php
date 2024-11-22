4d1e683eaa145c33fbf0ae21a4908044ac5780fa
juancruzgw <jgeslowski@gmail.com>
2024-11-21 21:55:32 -0300

    a acomodar todo :v

--- src/Vista/menu/action.php
+++ src/Vista/menu/action.php
@@ -1,5 +1,6 @@
 <?php
 include '../../../config.php';
+header('application/json');
 $abmMenu = new abmMenu();
 $abmUsuarioRol = new abmUsuarioRol();
 $usuario = $session->getUsuario();
--- src/Vista/menu/index.php
+++ src/Vista/menu/index.php
@@ -1,4 +1,37 @@
-    <?php include '../estructura/cabeceraSegura.php' ?>
+    <?php include '../estructura/cabeceraSegura.php';?>
+
+
+    <script>
+        
+        $(document).ready(function() {
+            $.ajax({
+                url: './action.php',
+                type: 'GET',
+                dataType: 'json',
+                success: function(response) {
+                    console.log(response);
+                    
+                    let menu = response.map(item => ({
+                        descripcion: item.medescripcion,
+                        nombre: item.menombre
+                    }));
+
+                    $('.deposito-menu').html('');
+                    
+                    menu.forEach(descripcion => {
+                        $('.deposito-menu').append(`<a href="${descripcion.descripcion}"><button class="btn btn-outline-primary">${descripcion.nombre}</button></a>`);
+                    });
+                },
+                error: function(xhr, status, error) {
+                    console.log('Error al cargar los datos.');
+                }
+            });
+        });
+
+
+    </script>
+
+    
 
     <div class="container-sm min-vh-100">  
 
@@ -13,8 +46,8 @@
         
     </div>
 
-
-    <script>
+    
+    <!-- <script>
     $(document).ready(function() {
 
         function mostrarMenues() {
@@ -905,4 +938,4 @@
         });
     }
             
-</script>
+</script> -->
